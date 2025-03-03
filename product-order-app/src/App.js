641e71f3c4ab57b79c7422149636e2b844367e2d
yuvrajpradhan <Yuvraj.pradhan.2004@gmail.com>
2025-03-03 23:23:14 +0530

    segment issue fixed

--- product-order-app/src/App.js
+++ product-order-app/src/App.js
@@ -60,10 +60,10 @@ const ProductOrderApp = () => {
     }
 
     emailjs.send(
-      'service_rz7rjr1',
+      'service_1v5gxov',
       'template_xhlfgdi',
       {
-        to_email: 'yuvraj.pradhan.2004@gmail.com',
+        to_email: 'yogeshdatia@gmail.com',
         firm_name: formData.firmName,
         mobile_number: formData.mobileNumber,
         city: formData.city,
